b85d50b9d52a2f7fc5135f2afb0e70a7bbac468d
Tristan <nocent.tristan@gmail.com>
2025-10-15 22:31:47 +0200

    header plus layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "@/components/Header";
+
+const Page = () => {
+    return (
+        <main className="wrapper page">
+            <Header title="All videos" subHeader="Public Library"/>
+            <h1 className="text-2xl font-karla">
+                Welcome to Loom Clone
+            </h1>
+        </main>
+    )
+}
+
+export default Page
